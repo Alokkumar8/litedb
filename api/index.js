7af8daf95bf03d3a116556e12881c99caa6881c7
alok <alokkumar.jnv10@gmail.com>
2025-06-25 12:27:49 +0530

    add parse

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
+app.use(express.urlencoded({ extended: true })); // To parse x-www-form-urlencoded body
 
 app.use(express.static("public"));
 
